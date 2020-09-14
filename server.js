5961431e22eb0e179226532da42a6b6120299331
Rittick chakraborty <rc.laden1997@gmail.com>
2020-09-14 13:56:51 +0530

    very basic routing inside main file

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+app.get('/api/v1/bootcamps/', (req, res) => {
+  res.status(200).json({ success: true, msg: 'show all bootcamps' });
+});
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: 'Fetching a bootcamp ' + req.params.id });
+});
+app.post('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(201).json({ success: true, msg: 'bootcamps created' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `updating the bootcamp ${req.params.id}` });
+});
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `bootcamp ${req.params.id} deleted` });
+});
 
 const PORT = process.env.PORT || 5000;
 
